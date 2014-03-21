94f2e975ed4c8281b5ceeb0e4276c31d833aa710
Moszkva <szecsi44@freemail.hu>
2014-03-21 13:47:51 +0100

    Adding test for route prefix handling

--- tests/Moszkva/Angie/Test/LaravelRouteParserTest.php
+++ tests/Moszkva/Angie/Test/LaravelRouteParserTest.php
@@ -14,6 +14,9 @@ class LaravelRouteParserTest extends \TestCase
 		\Route::get('/test3', 'AngieTestController3@someGetMethod');
 		\Route::get('/test/test3', 'AngieTestController3@someShowMethod');
 		\Route::put('/test3/{id}/edit', 'AngieTestController3@somePutMethod');
+		
+		
+		\Route::get('/test4/{id}/edit', array('prefix' => 'testprefix', 'uses' => 'AngieTestController4@someGetMethod'));
 	}
 	
 	public function testGetRouteCollection()
@@ -36,8 +39,10 @@ class LaravelRouteParserTest extends \TestCase
 		$this->assertContains('test3', $controllersURLs['AngieTestController3']);
 		$this->assertContains('test/test3', $controllersURLs['AngieTestController3']);
 		
-		$this->assertNotContains('/test3/{id}/edit', $controllersURLs['AngieTestController3']);
+		$this->assertNotContains('/test3/:id/edit', $controllersURLs['AngieTestController3']);
 		$this->assertNotContains('/test2/:test2', $controllersURLs['AngieTest2Controller']);
+		
+		$this->assertContains('testprefix/test4/:id/edit', $controllersURLs['AngieTestController4']);
 	}
 	
 	public function testGetMethodsByController()
